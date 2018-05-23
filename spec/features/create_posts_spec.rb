1cb1c5f366f637ab17b22b32b3dd6bdbb7d7b0b3
Ben <ben.challenor@btopenworld.com>
2018-05-23 14:24:30 +0100

    Add test for creating a note

--- spec/features/create_posts_spec.rb
+++ spec/features/create_posts_spec.rb
@@ -1,1 +1,13 @@
 require 'rails_helper.rb'
+
+feature 'Creating notes' do
+  scenario 'can create note' do
+    visit '/'
+    click_link 'Create Note'
+    fill_in 'Title', with: 'title'
+    fill_in 'Note', with: 'note'
+    click_button 'Update Note'
+    expect(page).to have_content('title')
+    expect(page).to have_content('note')
+  end
+end
